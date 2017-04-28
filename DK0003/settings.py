01347fe698b5a81746e75772032980b19af4c261
Derekzxz <zhouchuzhou@live.cn>
2017-04-28 13:05:01 +0800

    ALLOW_HOST_ADD
    ADD "dk555.pythonanywhere.com" to ALLOWED_HOSTS in settings.py

--- DK0003/settings.py
+++ DK0003/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '368e6a)=thhwl259#_+pf-ri!ywslynh5x9k@7)$&qzryk$)qa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dk555.pythonanywhere.com',]
 
 
 # Application definition
